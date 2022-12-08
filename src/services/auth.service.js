0cf22fff8d31fa15bdc05a8c49c7e05357cd063f
chienbn9x <chienbn9x@gmail.com>
2022-12-09 00:03:00 +0700

    2022/12/08

---
+++ src/services/auth.service.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth";
+
+const login = (username, password) => {
+  return axios.post(
+    API_URL + "signin",
+    { username, password }
+  ).then((response) => {
+    if (response.data.accessToken) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+
+    return response.data;
+  });
+}
+
+const logout = () => {
+  localStorage.removeItem("user");
+}
+
+const register = (username, email, password) => {
+  return axios.post(
+    API_URL + "signup",
+    { username, email, password }
+  );
+}
+
+export default { login, logout, register };
